4567efca259f351f29b096da32f89f4a39cd57d8
manupanand <manupanandindianinvestor@gmail.com>
2025-03-26 18:52:09 +0530

    debug

--- k8s-infra-selfmanaged/modules/k8s/main.tf
+++ k8s-infra-selfmanaged/modules/k8s/main.tf
@@ -1,7 +1,7 @@
 
 output "subnet" {
-  value = data.aws_subnet.kube_subnet.id
+  value = data.aws_subnet.kube_subnet
 }
-output "subnet_cidr_block" {
-  value = [for s in data.aws_subnet.kube_subnet : s.cidr_block]
-}
+# output "subnet_cidr_block" {
+#   value = for s in data.aws_subnet.kube_subnet : s.cidr_block
+# }
