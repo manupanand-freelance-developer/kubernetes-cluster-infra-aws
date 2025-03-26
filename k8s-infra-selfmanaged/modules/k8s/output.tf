e4c394a12d74241b2cc754144f20602df540e291
manupanand <manupanandindianinvestor@gmail.com>
2025-03-26 18:57:21 +0530

    cidr output

--- k8s-infra-selfmanaged/modules/k8s/output.tf
+++ k8s-infra-selfmanaged/modules/k8s/output.tf
@@ -1,5 +1,5 @@
 output "subnet" {
-  value = data.aws_subnet.kube_subnet
+  value =[ for s in data.aws_subnet.kube_subnet[*] : s]
 }
 # output "subnet_cidr_block" {
 #   value = for s in data.aws_subnet.kube_subnet : s.cidr_block
