e79ffa2c2df4f5fc0f726147bcf1461019456fbb
manupanand <manupanandindianinvestor@gmail.com>
2025-03-26 19:05:10 +0530

    run security gropu

--- k8s-infra-selfmanaged/modules/k8s/security-group.tf
+++ k8s-infra-selfmanaged/modules/k8s/security-group.tf
@@ -1,37 +1,37 @@
-# resource "aws_security_group" "kube_control_plane" {
-#     description = "Security group for control plane"
-#     name        =  "${var.env}-kube-control-plane-sg"
-#     vpc_id      =   data.aws_vpc.private_vpc.id 
+resource "aws_security_group" "kube_control_plane" {
+    description = "Security group for control plane"
+    name        =  "${var.env}-kube-control-plane-sg"
+    vpc_id      =   data.aws_vpc.private_vpc.id 
   
 
-# #ssh port
-# ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "TCP"
-#     cidr_blocks  =["0.0.0.0/0"] 
-# }
+#ssh port
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks  =["0.0.0.0/0"] 
+}
 
-# # kube main ports
-#   dynamic "ingress" {
-#     for_each = var.cp_ingress
-#     content {
-#       from_port     = tonumber(split("-",ingress.value.port)[0])
-#       to_port       = tonumber(split("-",ingress.value.port)[length(split("-",ingress.value.port))-1])
-#       protocol      = "TCP"
-#       cidr_blocks   = ingress.value.cidr #var.kube_subnet_cidr
-#     }
-#   }
-# # TCP	Inbound	6443	    Kubernetes API server	    All
-# # TCP	Inbound	2379-2380	etcd server client API	    kube-apiserver, etcd
-# # TCP	Inbound	10250	    Kubelet API	                Self, Control plane
-# # TCP	Inbound	10259	    kube-scheduler	            Self
-# # TCP	Inbound	10257	    kube-controller-manager	    Self
+# kube main ports
+  dynamic "ingress" {
+    for_each = var.cp_ingress
+    content {
+      from_port     = tonumber(split("-",ingress.value.port)[0])
+      to_port       = tonumber(split("-",ingress.value.port)[length(split("-",ingress.value.port))-1])
+      protocol      = "TCP"
+      cidr_blocks   = ingress.value.cidr #var.kube_subnet_cidr data.aws_subnet.kube_subnet.cidr_block
+    }
+  }
+# TCP	Inbound	6443	    Kubernetes API server	    All
+# TCP	Inbound	2379-2380	etcd server client API	    kube-apiserver, etcd
+# TCP	Inbound	10250	    Kubelet API	                Self, Control plane
+# TCP	Inbound	10259	    kube-scheduler	            Self
+# TCP	Inbound	10257	    kube-controller-manager	    Self
 
-#         tags={
-#             Name="${var.env}-kube-control-plane-sg"
-#         }
-# }
+        tags={
+            Name="${var.env}-kube-control-plane-sg"
+        }
+}
 
 # resource "aws_security_group" "kube_worker" {
 
