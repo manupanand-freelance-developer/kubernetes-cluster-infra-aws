b403ec75df309276929a2eeb209c357e77d7ff46
manupanand <manupanandindianinvestor@gmail.com>
2025-03-31 18:25:28 +0530

    update code test

--- k8s-infra-selfmanaged/main.tf
+++ k8s-infra-selfmanaged/main.tf
@@ -4,16 +4,16 @@ module "kube" {
   env               = var.env
   vpc_name          = var.vpc["vpc_name"]
   subnet_name       = var.vpc["subnet_name"] 
-  cp_ingress        = var.cp_ingress
-  cp_egress         = var.cp_egress
-  worker_ingress    = var.worker_ingress
-  worker_egress     = var.worker_egress
+  # cp_ingress        = var.cp_ingress
+  # cp_egress         = var.cp_egress
+  # worker_ingress    = var.worker_ingress
+  # worker_egress     = var.worker_egress
   name              = each.key
   instance_type     = each.value["instance_type"]
   policy_name       = each.value["policy_name"]
   volume_size       = each.value["volume_size"]
   aws_ami_id        = var.aws_ami_id
   aws_user          = var.aws_user 
-  aws_password      = var.aws_password 
+  aws_password      = var.aws_password
   
 }
