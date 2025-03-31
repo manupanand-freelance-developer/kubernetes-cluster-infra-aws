c543a60b08f0bd55763999cad2e5caceb34c77f6
manupanand <manupanandindianinvestor@gmail.com>
2025-03-31 21:49:54 +0530

    cleard bug

--- k8s-infra-selfmanaged/modules/control-plane/output.tf
+++ k8s-infra-selfmanaged/modules/control-plane/output.tf
@@ -1,6 +1,3 @@
 output "aws_control_plane_private_ip" {
   value = aws_instance.instance_control_plane.private_ip
 }
-output "control_plane_security_group_id" {
-  value = aws_security_group.kube_control_plane.id
-}
