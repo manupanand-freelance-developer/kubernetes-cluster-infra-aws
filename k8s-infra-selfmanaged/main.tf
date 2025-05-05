51d5faff4e1e50e96dc9c4726f12ab54fca800b0
manupanand <manupanandindianinvestor@gmail.com>
2025-05-05 09:38:41 +0530

    updated code

--- k8s-infra-selfmanaged/main.tf
+++ k8s-infra-selfmanaged/main.tf
@@ -29,26 +29,26 @@ module "control-plane" {
   private_security_group_id= module.security_groups.control_plane_security_group
   
 }# depend on null and wait here
-# resource "time_sleep" "delay" {
-#   depends_on = [ module.control-plane]
-#   create_duration = "180s"
-# }
+resource "time_sleep" "delay" {
+  depends_on = [ module.control-plane]
+  create_duration = "60s"
+}
 
-# module "worker" {
-#   source                    = "./modules/worker"
-#   depends_on                = [time_sleep.delay]
-#   for_each                  = var.worker_instance
-#   env                       = var.env
-#   kube_subnet_id            = module.security_groups.kube_subnet_id
-#   name                      = each.key
-#   instance_type             = each.value["instance_type"]
-#   policy_name               = each.value["policy_name"]
-#   volume_size               = each.value["volume_size"]
-#   aws_ami_id                = var.aws_ami_id
-#   aws_user                  = var.aws_user 
-#   aws_password              = var.aws_password
-#   remote_ip                 = module.control-plane["master_node_1"].aws_control_plane_private_ip 
-#   private_security_group_id = module.security_groups.worker_security_group
+module "worker" {
+  source                    = "./modules/worker"
+  depends_on                = [time_sleep.delay]
+  for_each                  = var.worker_instance
+  env                       = var.env
+  kube_subnet_id            = module.security_groups.kube_subnet_id
+  name                      = each.key
+  instance_type             = each.value["instance_type"]
+  policy_name               = each.value["policy_name"]
+  volume_size               = each.value["volume_size"]
+  aws_ami_id                = var.aws_ami_id
+  aws_user                  = var.aws_user 
+  aws_password              = var.aws_password
+  remote_ip                 = module.control-plane["master_node_1"].aws_control_plane_private_ip 
+  private_security_group_id = module.security_groups.worker_security_group
   
-# }
+}
 
